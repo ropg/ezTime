f8467318e9bb966f6c84c53d9878e4c4c48e2d0b
Verkehrsrot <cyberman@obh.snafu.de>
2021-05-08 15:19:36 +0200

    Update ezTime.cpp

--- src/ezTime.cpp
+++ src/ezTime.cpp
@@ -1164,6 +1164,11 @@ void Timezone::setTime(const time_t t, const uint16_t ms /* = 0 */) {
 	_time_status = timeSet;
 }
 
+void Timezone::syncToPPS() {
+	_last_sync_time ++;
+	_last_sync_millis = 0;
+}
+
 void Timezone::setTime(const uint8_t hr, const uint8_t min, const uint8_t sec, const uint8_t day, const uint8_t mnth, uint16_t yr) {
 	tmElements_t tm;
 	// year can be given as full four digit year or two digts (2010 or 10 for 2010);  
@@ -1505,6 +1510,7 @@ namespace ezt {
 	uint8_t setEvent(void (*function)(), time_t t /* = TIME_NOW */, const ezLocalOrUTC_t local_or_utc /* = LOCAL_TIME */) { return (defaultTZ->setEvent(function, t, local_or_utc)); }
 	void setTime(const uint8_t hr, const uint8_t min, const uint8_t sec, const uint8_t day, const uint8_t month, const uint16_t yr) { defaultTZ->setTime(hr, min, sec, day, month, yr); }
 	void setTime(time_t t) { defaultTZ->setTime(t); }
+	void syncToPPS(void) { defaultTZ->SyncToPPS(); }
 	uint8_t weekISO(time_t t /* = TIME_NOW */, const ezLocalOrUTC_t local_or_utc /* = LOCAL_TIME */) { return (defaultTZ->weekISO(t, local_or_utc)); }
 	uint8_t weekday(time_t t /* = TIME_NOW */, const ezLocalOrUTC_t local_or_utc /* = LOCAL_TIME */) { return (defaultTZ->weekday(t, local_or_utc)); }
 	uint16_t year(time_t t /* = TIME_NOW */, const ezLocalOrUTC_t local_or_utc /* = LOCAL_TIME */) { return (defaultTZ->year(t, local_or_utc)); } 
